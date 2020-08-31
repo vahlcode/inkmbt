a39df92f271237ccbb79e69cf975e902d4edd6fc
Valentine Elum <valentineelum@gmail.com>
2020-08-31 21:45:51 +0100

    Add files

---
+++ index.js
@@ -0,0 +1,23 @@
+//Author : Valentine Elum
+//For : Desyn Open Source
+//Twitter : @VahlCode
+//License : MIT License 
+
+
+module.exports = function inkmbt(number) {
+    if (number > 999 && number <= 999999) {
+        let output = Math.floor(number/1000)+'k';
+        return output;
+    } else if (number > 999999 && number <= 999999999) {
+        let output = Math.floor(number / 1000000)+'M';
+        return output;
+    } else if (number > 999999999 && number <= 999999999999) {
+        let output = Math.floor(number / 1000000000) +'B';
+        return output;
+    } else if (number > 999999999999) {
+        let output = Math.floor(number / 1000000000000) + 'T';
+        return output;
+    } else {
+        return number;
+    }
+}
